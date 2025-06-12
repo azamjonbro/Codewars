8f929dc53c59d52d33efe0917b57f43044dc115c
azamjonbro <azamjondasturchi11@gmail.com>
2025-06-13 00:05:33 +0500

    added

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -19,3 +19,29 @@ function isVow(a){
   });
 }
 
+
+
+
+//https://www.codewars.com/kata/5aa736a455f906981800360d/solutions/javascript
+
+// the best path
+// function feast(beast, dish) {
+// 	return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+// }
+
+// medium solution 
+// function feast(beast, dish) {
+// console.log(beast, dish)
+// let firstItem= beast.split("")[0]
+// let secondItem = beast.split("")[beast.length-1]
+
+// console.log(firstItem,secondItem)
+// if(dish.split('')[0]==firstItem&& dish.split("")[dish.length-1]==secondItem){
+//     return true
+// }
+// else{
+//     return false
+// }
+// }
+
+
